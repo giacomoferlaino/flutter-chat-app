125c8938fad50decee4552c061b460046c57cf47
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-08-18 21:28:46 +0200

    feat: Improved chat NewMessage for iOS

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -51,6 +53,9 @@ class ChatPage extends StatelessWidget {
           children: [
             Expanded(child: Messages(_getDatStream())),
             NewMessage(),
+            if (Platform.isIOS &&
+                MediaQuery.of(context).viewInsets.bottom == 0.0)
+              SizedBox(height: 35),
           ],
         ),
       ),
